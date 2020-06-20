61c318a72e1d9cb1739a3ce0ca5e50eddc53c3b8
Nick Holke <nickholke@gmail.com>
2020-06-20 10:51:51 -0700

    Added config and metadata custom hooks

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { ThemeProvider} from 'styled-components';
+// Hooks
+import { useMetaDataQuery } from 'hooks/useMetaDataQuery';
+
 // Styles
 import { GlobalStyles, darkTheme, lightTheme} from 'styles/GlobalStyles';
 
 const Layout = ({ children }) => {
-
+  const data = useMetaDataQuery();
+  console.log('data', data)
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
